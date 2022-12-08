e27a618b42c62483eb05aa63c8dc207929d0edb4
utsav-chadha <31806285+utsav-chadha@users.noreply.github.com>
2022-12-08 17:11:19 -0500

    .env added and server changes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 
+const express = require('express')
+require('dotenv').config()
+const connection = require('./config/db')
+const errorHandler = require('./middlewares/errorHandler')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+
+// body parser middlewares
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+// cors
+app.use(cors())
+
+// router
+app.use('/api', require('./routes/adRoutes'))
+app.use('/api/auth', require('./routes/authRoutes'))
