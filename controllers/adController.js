af6a187061e485ee947b6e11afc0770e7121b682
Puranjai Mendiratta <mendiratta.p@northeastern.edu>
2022-12-08 18:32:40 -0500

    Updated adController.js (added Get ads, get individiual ads, get item user)

--- controllers/adController.js
+++ controllers/adController.js
@@ -58,3 +58,47 @@ const postAd = asyncHandler(async (req, res) => {
     throw new Error('could not save your ad')
   }
 })
+
+// GET ADS
+const getAds = asyncHandler(async (req, res) => {
+    const ads = await AdModel.find({})
+  
+    if (!ads) {
+      res.status(404)
+      throw new Error('No ads data to show')
+    }
+  
+    res.json(ads)
+  })
+  
+  // GET INDIVIDUAL AD
+  const getAd = asyncHandler(async (req, res) => {
+    const id = req.params.id
+    const ad = await AdModel.findOne({ _id: id }).populate({
+      path: 'user',
+      select: '-password',
+    })
+  
+    if (!ad) {
+      res.status(404)
+      throw new Error('No item found')
+    }
+  
+    res.json(ad)
+  })
+  
+  // GET Item User
+  const itemUser = asyncHandler(async (req, res) => {
+    const { userId } = req.body
+  
+    const user = await AuthModel.findOne({ _id: userId })
+      .select('-password')
+      .select('-ads')
+  
+    if (!user) {
+      res.status(404)
+      throw new Error('No user found')
+    }
+  
+    res.json(user)
+  })
