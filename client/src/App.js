36a8cd36a0d7f9555dcd0c72260fcd1bfa637609
Bonie Sachdev <sachdev.bo@northeastern.edu>
2022-12-08 22:28:57 -0500

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/post"
+            element={
+              <ProtectedRoute>
+                <PostAd />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <PublicRoute>
+                <SignUp />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/activate/:token"
+            element={
+              <PublicRoute>
+                <ActivateAccount />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/change-password/:token"
+            element={
+              <PublicRoute>
+                <ChangePassword />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/find-account"
+            element={
+              <PublicRoute>
+                <FindAccount />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/myads"
+            element={
+              <ProtectedRoute>
+                <MyAds />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/update/item/:id"
+            element={
+              <ProtectedRoute>
+                <UpdateAd />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/item/:id" element={<Ad />} />
+        </Routes>
+        <Footer />
+      </Router>
+
+      <Toaster />
     </div>
   );
 }
